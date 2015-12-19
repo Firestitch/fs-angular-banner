ac2b897da7643f963920790e2bde3cc9238d58b7
Raymond <ray@firestitch.com>
2015-12-19 11:37:01 -0500

    commit

---
+++ js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "fs-angular-banner.directives:banner",
+      "shortName": "banner",
+      "type": "directive",
+      "moduleName": "fs-angular-banner",
+      "shortDescription": "",
+      "keywords": "action api area avatar banner bn-options callback class click clicked configure configures directive directives forms fs-angular-banner function headline href icon icons label located material options side subheadline submit table table-bordered table-striped top type type-hint type-hint-function variables-matrix"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
